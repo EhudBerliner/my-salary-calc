cd570d5eef9dd4501bf94c2593a58190c49d357a
EhudBerliner <ehud.berliner@gmail.com>
2026-01-07 12:15:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
 const CACHE_NAME = 'salary-calc-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-// התקנה ושמירה ב-Cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// שליפת נתונים מה-Cache כשאין אינטרנט
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
